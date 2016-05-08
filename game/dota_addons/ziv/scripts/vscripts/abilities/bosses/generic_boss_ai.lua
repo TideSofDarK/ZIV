e8b676ab82a2f11cf92f0023ea96bb3409728d19
TideSofDarK <tidesmain@gmail.com>
2016-05-09 01:02:31 +0500

    Boss states

--- game/dota_addons/ziv/scripts/vscripts/abilities/bosses/generic_boss_ai.lua
+++ game/dota_addons/ziv/scripts/vscripts/abilities/bosses/generic_boss_ai.lua
@@ -1,3 +1,13 @@
-function Start( keys )
+BOSS_STATE_IDLE = 0
+BOSS_STATE_CHASING = 1
+BOSS_STATE_CASTING = 2
+
+function AI:BossStart( keys )
 	local caster = keys.caster
+end
+
+function AI:BossCasting( unit )
+	Timers:CreateTimer(function (  )
+		
+	end)
 end
