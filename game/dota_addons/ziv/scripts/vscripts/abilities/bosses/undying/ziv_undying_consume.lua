a1d37cf0a73f75afae6582154161b596cadd7363
TideSofDarK <tidesmain@gmail.com>
2016-05-10 04:09:02 +0500

    Consume fixes

--- game/dota_addons/ziv/scripts/vscripts/abilities/bosses/undying/ziv_undying_consume.lua
+++ game/dota_addons/ziv/scripts/vscripts/abilities/bosses/undying/ziv_undying_consume.lua
@@ -3,22 +3,20 @@ function Consume( keys )
 	local target = keys.target
 	local ability = keys.ability
 
-	ability:SetChanneling(true)
-
 	Timers:CreateTimer(0.1, function (  )
 		if ability:IsChanneling() then
-			ParticleManager:CreateParticle("particles/units/heroes/hero_skeletonking/wraith_king_vampiric_aura_lifesteal.vpcf", PATTACH_ABSORIGIN, caster)
+			ParticleManager:CreateParticle("particles/units/heroes/hero_skeletonking/wraith_king_vampiric_aura_lifesteal.vpcf", PATTACH_OVERHEAD_FOLLOW, caster)
 			StartAnimation(caster, {duration=0.6, activity=ACT_DOTA_ATTACK, rate=1.5})
 
 			EndAnimation(target)
-			StartAnimation(target, {duration=-1, activity=ACT_DOTA_DISABLED, rate=1.5})
+			StartAnimation(target, {duration=0.5, activity=ACT_DOTA_DISABLED, rate=2.0})
 
 			ability:ApplyDataDrivenModifier(caster, target, "modifier_undying_consume", {duration = 0.5})
-			DealDamage(caster, target, target:GetMaxHealth() * GetSpecial(ability, "hp_percent"), DAMAGE_TYPE_PHYSICAL)
+			DealDamage(caster, target, target:GetHealth() * GetSpecial(ability, "hp_percent"), DAMAGE_TYPE_PHYSICAL)
 
 			return 0.5
 		else
-			EndAnimation(target)
+			-- EndAnimation(target)
 		end
 	end)
 end
