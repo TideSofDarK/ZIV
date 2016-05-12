6933e927d7d2aaec4811f5591a265222061a8734
K1llMan <k1llmanws@yandex.ru>
2016-05-12 23:52:09 +0300

    Minimap events, refactoring

---
+++ content/dota_addons/ziv/panorama/scripts/custom_game/minimap/marks/events.js
@@ -0,0 +1,20 @@
+"use strict";
+var imagePath = "file://{images}/custom_game/minimap/event_icons/";
+
+function GetPlayerColor( PlayerID )
+{
+	var color = Players.GetPlayerColor( PlayerID ).toString(16);
+	color = color.substring(6, 8) + color.substring(4, 6) + color.substring(2, 4) + color.substring(0, 2);
+	return "#" + color + ";";
+}
+
+function FormEvent( type, player, entity )
+{
+	$( "#Icon" ).SetImage( imagePath + type + ".png" );
+	$( "#Icon" ).style.washColor = GetPlayerColor( player );
+}
+
+(function()
+{ 
+	$.GetContextPanel().FormEvent = FormEvent;
+})();
