ac7c9f4794efae82fa4c30c76064baf40c7f74ed
TideSofDarK <tidesmain@gmail.com>
2016-05-28 05:03:33 +0500

    Started to rework knight

---
+++ game/dota_addons/ziv/scripts/vscripts/abilities/heroes/knight/ziv_knight_orb.lua
@@ -0,0 +1,66 @@
+function NewOrb( keys )
+	local keys = keys
+
+	local caster = keys.caster
+	local target = keys.target
+	local ability = keys.ability
+
+	ability.orbs = ability.orbs or {}
+
+	local particle = ParticleManager:CreateParticle("particles/heroes/knight/knight_slash_orb.vpcf",PATTACH_CUSTOMORIGIN_FOLLOW,caster)
+	ParticleManager:SetParticleControlEnt(particle,0,caster,PATTACH_POINT_FOLLOW,"attach_attack1",caster:GetAbsOrigin(),false)
+
+	table.insert(ability.orbs, particle)
+
+	-- StartRuneCooldown(ability,"ziv_beastmaster_cleave_speed",caster)
+
+	-- ability.activity = ability.activity or ACT_DOTA_ATTACK_EVENT
+	-- if ability.activity == ACT_DOTA_ATTACK_EVENT then
+	-- 	ability.activity = ACT_DOTA_ATTACK2
+	-- 	keys.attack_particle = "particles/units/heroes/hero_ursa/ursa_claw_right.vpcf"
+	-- else
+	-- 	ability.activity = ACT_DOTA_ATTACK_EVENT
+	-- 	keys.attack_particle = "particles/units/heroes/hero_ursa/ursa_claw_left.vpcf"
+	-- end
+
+	-- keys.activity = ability.activity
+
+	-- keys.duration = 0.1
+	-- keys.rate = 3.0
+	-- keys.base_attack_time = 0.25
+
+	-- keys.on_impact = (function ( caster )
+	-- 	local units = FindUnitsInCone(caster:GetAbsOrigin(), caster:GetForwardVector(), 300, 300 + GRMSC("ziv_beastmaster_cleave_aoe", caster), caster:GetTeamNumber(), DOTA_UNIT_TARGET_TEAM_ENEMY, DOTA_UNIT_TARGET_ALL, DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES, FIND_ANY_ORDER)
+		
+	-- 	for k,v in pairs(units) do
+	-- 		ParticleManager:CreateParticle("particles/units/heroes/hero_riki/riki_backstab_hit_blood.vpcf",PATTACH_ABSORIGIN,v)
+	-- 		ParticleManager:CreateParticle("particles/units/heroes/hero_dazzle/dazzle_poison_touch_blood.vpcf",PATTACH_ABSORIGIN,v)
+	-- 		DealDamage(caster,v,caster:GetAverageTrueAttackDamage(), DAMAGE_TYPE_PHYSICAL)
+	-- 	end
+	-- end)
+
+	-- SimulateMeleeAttack( keys )
+end
+
+function LaunchOrb( keys )
+	local keys = keys
+	
+	local caster = keys.caster
+	local target = keys.target_points[1]
+	local ability = keys.ability
+
+	-- ability.orbs[#ability.orbs] = s
+
+	keys.effect = "particles/heroes/knight/knight_orb_attack.vpcf"
+	keys.projectile_speed = 600
+	keys.duration = 0.1
+	keys.rate = 3.0
+	keys.base_attack_time = 0.15
+	keys.attack_sound = "Hero_DrowRanger.Attack"
+	keys.interruptable = false
+	keys.on_impact = (function ( caster )
+
+	end)
+
+	SimulateRangeAttack(keys)
+end
