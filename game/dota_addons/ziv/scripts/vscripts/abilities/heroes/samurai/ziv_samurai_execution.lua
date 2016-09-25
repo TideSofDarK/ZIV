43a69b3d95f921e53c2e73e6791a8cc2b170a3b2
TideSofDarK <tidesmain@gmail.com>
2016-09-26 00:45:47 +0500

    Samurai's skills cleanup, new particle for Execution with cold modifier

--- game/dota_addons/ziv/scripts/vscripts/abilities/heroes/samurai/ziv_samurai_execution.lua
+++ game/dota_addons/ziv/scripts/vscripts/abilities/heroes/samurai/ziv_samurai_execution.lua
@@ -7,34 +7,43 @@ function StartAttack( keys )
 
 	StartAnimation(caster, {duration=0.5, activity=ACT_DOTA_ATTACK_EVENT, rate=2.2})
 
-	local p_endx = 'particles/econ/items/juggernaut/jugg_sword_fireborn_odachi/jugg_crit_blur_fb_odachi.vpcf'
-	local p1 = ParticleManager:CreateParticle(p_endx, PATTACH_ABSORIGIN_FOLLOW, caster)
-	ParticleManager:SetParticleControl(p1, 0, caster:GetOrigin())
+	local cold = caster:HasModifier("modifier_cold_touch") 
+
+	local trail = "particles/heroes/samurai/samurai_execution_trail_fire.vpcf"
+	local fx = "particles/units/heroes/hero_rattletrap/rattletrap_rocket_flare_explosion_flash_c.vpcf"
+
+	if cold then
+		trail = "particles/heroes/samurai/samurai_execution_trail_ice.vpcf"
+		fx = "particles/heroes/samurai/samurai_execution_frost_earth.vpcf"
+	end
+
+	local particle = ParticleManager:CreateParticle(trail, PATTACH_ABSORIGIN_FOLLOW, caster)
+	ParticleManager:SetParticleControl(particle, 0, caster:GetOrigin())
 
 	caster:EmitSound("Hero_DragonKnight.PreAttack")
 
 	Timers:CreateTimer(0.09, function()
-		p1 = ParticleManager:CreateParticle('particles/heroes/samurai/samurai_execution_cleave.vpcf', PATTACH_CUSTOMORIGIN, caster)
-		ParticleManager:SetParticleControl(p1, 0, target)
+		particle = ParticleManager:CreateParticle('particles/heroes/samurai/samurai_execution_cleave.vpcf', PATTACH_CUSTOMORIGIN, caster)
+		ParticleManager:SetParticleControl(particle, 0, target)
 
-		p1 = ParticleManager:CreateParticle("particles/heroes/samurai/samurai_execution_cleave_dust.vpcf", PATTACH_CUSTOMORIGIN, caster)
-		ParticleManager:SetParticleControl(p1, 0, target)
+		particle = ParticleManager:CreateParticle("particles/heroes/samurai/samurai_execution_cleave_dust.vpcf", PATTACH_CUSTOMORIGIN, caster)
+		ParticleManager:SetParticleControl(particle, 0, target)
 
 		local earth_particle_name = "particles/units/heroes/hero_ursa/ursa_earthshock_soil.vpcf"
 		local damage_type = DAMAGE_TYPE_PHYSICAL
 		if caster:HasModifier("modifier_cold_touch") then
-			earth_particle_name = "particles/heroes/samurai/samurai_execution_frost_earth.vpcf"
+			earth_particle_name = fx
 
 			damage_type = DAMAGE_TYPE_COLD
 		else
-			local fire_effect = ParticleManager:CreateParticle("particles/units/heroes/hero_rattletrap/rattletrap_rocket_flare_explosion_flash_c.vpcf", PATTACH_ABSORIGIN, caster)
+			local fire_effect = ParticleManager:CreateParticle(fx, PATTACH_ABSORIGIN, caster)
 			ParticleManager:SetParticleControl(fire_effect, 3, target)
 		
 			damage_type = DAMAGE_TYPE_FIRE
 		end
 
-		p1 = ParticleManager:CreateParticle(earth_particle_name, PATTACH_CUSTOMORIGIN, caster)
-		ParticleManager:SetParticleControl(p1, 0, target)
+		particle = ParticleManager:CreateParticle(earth_particle_name, PATTACH_CUSTOMORIGIN, caster)
+		ParticleManager:SetParticleControl(particle, 0, target)
 
 		local units = FindUnitsInRadius(caster:GetTeamNumber(), target, nil, ability:GetSpecialValueFor("radius") + GRMSC("ziv_samurai_execution_aoe", caster), DOTA_UNIT_TARGET_TEAM_ENEMY, DOTA_UNIT_TARGET_ALL, DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES, FIND_ANY_ORDER, false)
 
