f0303ec44572a0d30b11680e0ed974d1e9186398
K1llMan <k1llmanws@yandex.ru>
2016-10-28 21:25:50 +0300

    Boss health percentage, lock stage, styling

--- content/dota_addons/ziv/panorama/scripts/custom_game/ingame_ui_debug_panel.js
+++ content/dota_addons/ziv/panorama/scripts/custom_game/ingame_ui_debug_panel.js
@@ -8,6 +8,43 @@ var states = {
 	chasing: 2
 }
 
+function hookSliderChange(panel, callback, onComplete) {
+    var shouldListen = false;
+    var checkRate = 0.1;
+    var currentValue = panel.value;
+
+    var inputChangedLoop = function() {
+        // Check for a change
+        if(currentValue != panel.value) {
+            // Update current string
+            currentValue = panel.value;
+
+            // Run the callback
+            callback(panel, currentValue);
+        }
+
+        if(shouldListen) {
+            $.Schedule(checkRate, inputChangedLoop);
+        }
+    }
+
+    panel.SetPanelEvent('onmouseover', function() {
+        // Enable listening, and monitor the field
+        shouldListen = true;
+        inputChangedLoop();
+    });
+
+    panel.SetPanelEvent('onmouseout', function() {
+        // No longer listen
+        shouldListen = false;
+
+        // Check the value once more
+        inputChangedLoop();
+
+        // When we complete
+        onComplete(panel, currentValue);
+    });
+}
 
 function OnDropDownChanged() {
 	selectedBoss = $("#BossesDropDown").GetSelected().id;
@@ -34,11 +71,15 @@ function ChangeBossState() {
 }
 
 function LockState() {
-
+	GameEvents.SendCustomGameEventToServer("ziv_debug_change_boss_lock_state", { lock:  $("#LockState").checked });
 }
 
 function Toggle() {
 	$.GetContextPanel().ToggleClass("WindowClosed");
+}
+
+function onHealthChange(panel, value) {
+	GameEvents.SendCustomGameEventToServer("ziv_debug_change_boss_health", { "health" : value });
 }
 
 (function() {
@@ -63,6 +104,8 @@ function Toggle() {
     	$("#BossesStateDropDown").AddOption(stateEntry);
 	}
 	$("#BossesStateDropDown").SetSelected("idle");
+
+	hookSliderChange($("#BossHealth"), onHealthChange, function(){})
 
 	Game.AddCommand("+ZIVShowDebugPanel", Toggle, "", 0); 
 	Game.AddCommand("-ZIVShowDebugPanel", undefined, "", 0); 
